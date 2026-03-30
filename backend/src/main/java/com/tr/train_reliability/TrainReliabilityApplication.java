58057be95dbb1ad60170db92046b21dac7deda59
Kazenoyama <147516635+Kazenoyama@users.noreply.github.com>
2026-03-30 09:23:13 +0200

    Start project with spring initializr

---
+++ backend/src/main/java/com/tr/train_reliability/TrainReliabilityApplication.java
@@ -0,0 +1,13 @@
+package com.tr.train_reliability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainReliabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainReliabilityApplication.class, args);
+	}
+
+}
